7c3c4e424900f5720eae6e113d163d616302350a
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-03-18 22:10:33 +0900

    init

---
+++ src/main/kotlin/com/goofy/localcache/starter/GoofyLocalCacheStarterApplication.kt
@@ -0,0 +1,11 @@
+package com.goofy.localcache.starter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoofyLocalCacheStarterApplication
+
+fun main(args: Array<String>) {
+    runApplication<GoofyLocalCacheStarterApplication>(*args)
+}
